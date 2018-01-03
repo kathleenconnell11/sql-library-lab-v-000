567fd8c98389e2e3e260eea7af208b3a6b44efe8
Kathleen Connell <kathleenconnell11@gmail.com>
2018-01-03 17:20:45 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT title, character FROM series WHERE character = 'human' ORDER BY character LIMIT 1;"
+  ""
 end
 
 def select_character_names_and_number_of_books_they_are_in
