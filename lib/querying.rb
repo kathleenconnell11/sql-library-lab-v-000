cefebdc66777d1f35b66d5ef4dc55b1119a51f55
Kathleen Connell <kathleenconnell11@gmail.com>
2018-01-03 17:43:34 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,8 +18,10 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series INNER JOIN series ON characters.series_id = series.id GROUP BY (series.title)
-  ORDER BY COUNT(characters.species) DESC HAVING characters.species = 'human' LIMIT 1;"
+  "SELECT series.title FROM series INNER JOIN series ON characters.series_id = series.id 
+  WHERE characters.species = 'human'
+  GROUP BY (series.title)
+  ORDER BY COUNT(characters.species) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
