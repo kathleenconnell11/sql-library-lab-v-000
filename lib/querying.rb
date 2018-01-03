a718e86249b7ea8975da9f15f567952387d17321
Kathleen Connell <kathleenconnell11@gmail.com>
2018-01-03 17:15:08 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,18 +1,19 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  ""
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto FROM characters ORDER BY motto LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  ""
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name FROM authors LEFT JOIN subgenres.name 
+  ON authors.id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
